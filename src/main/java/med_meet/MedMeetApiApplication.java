65dc486e293af92e6894ffbcefd491ef60317993
patinodeveloper <pepe_toni_02@hotmail.com>
2024-11-05 14:51:06 -0600

    Initial Commit: project created and mysql connection enabled

---
+++ src/main/java/med_meet/MedMeetApiApplication.java
@@ -0,0 +1,13 @@
+package med_meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedMeetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedMeetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med_meet/MedMeetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med_meet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedMeetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
